1d29fba2440bbea94f8a526dd71c13ad781e029b
cxu123 <496355474@qq.com>
2018-08-14 15:28:51 +0800

    引入log4j和配置

--- src/main/java/com/luck/springboot/App.java
+++ src/main/java/com/luck/springboot/App.java
@@ -3,15 +3,18 @@ package com.luck.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.luck.springboot.listener.SpringBootLIstener;
+
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-       SpringApplication.run(App.class, args);
-    }
+public class App {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(App.class);
+		// SpringBoot启动监听器
+		app.addListeners(new SpringBootLIstener());
+		app.run(args);
+	}
 }
--- src/main/java/com/luck/springboot/service/impl/GetGatewayInfoImpl.java
+++ src/main/java/com/luck/springboot/service/impl/GetGatewayInfoImpl.java
@@ -1,7 +1,13 @@
 package com.luck.springboot.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import com.luck.springboot.service.GetGatewayInfo;
 
+@Service
 public class GetGatewayInfoImpl  implements GetGatewayInfo{
 
+
+	
 }
